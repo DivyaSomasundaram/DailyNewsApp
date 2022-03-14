072542ef63c67d85d461e217c04d8af18866701d
Divya <divyasomu27@gmail.com>
2022-03-14 14:25:44 +0530

    Fixed linking issue

--- DailyNewsAppUITests/DailyNewsAppUITests.swift
+++ DailyNewsAppUITests/DailyNewsAppUITests.swift
@@ -23,12 +23,12 @@ class DailyNewsAppUITests: XCTestCase {
         XCTAssert(app.staticTexts["Updated on:"].exists)
     }
     
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
-    }
+//    func testLaunchPerformance() throws {
+//        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
+//            // This measures how long it takes to launch your application.
+//            measure(metrics: [XCTApplicationLaunchMetric()]) {
+//                XCUIApplication().launch()
+//            }
+//        }
+//    }
 }
